0e3e6dd5ce32ecb43e62f910ea1122cd7df68de8
ionquen <ionqwen@gmail.com>
2022-10-06 22:58:25 +0300

    1

--- root/en/bingo/index_.php
+++ root/en/bingo/index_.php
@@ -1,1 +1,1 @@
-{"textStyle":"Text style","cellStyle":"Cell style","resetStyles":"Reset cell styles","useCheckMark":"Use check mark","autoSize":"AutoSize","editorMode":"Editor mode","selectionMode":"Selection mode","columns":"Number of columns","rows":"Number of rows"}
+{"textStyle":"Text style","cellStyle":"Cell style","resetStyles":"Reset cell styles","useCheckMark":"Use check mark","autoSize":"AutoSize","editorMode":"Editor mode","selectionMode":"Selection mode","columns":"Number of columns","rows":"Number of rows","nameStyled":"Stylize the name","fullSizeImage":"Entire images in a cell"}
